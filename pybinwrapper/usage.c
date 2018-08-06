077b8c23779477197dec9f46cf4fbf724fad88cb
Markus Johansson <markus@parkus.se>
2018-08-06 17:10:20 -0400

    Fixed spaces

--- pybinwrapper/usage.c
+++ pybinwrapper/usage.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("Usage:\n");
+    printf("Usage:\n");
     printf("$ %s                            # Run embedded command\n", argv[0]);
     printf("$ %s <command> [args]           # Embed new command in this file\n", argv[0]);
     printf("$ %s <command> [args] > new.py  # Create new file with embedded command\n", argv[0]);
